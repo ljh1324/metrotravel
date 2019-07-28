16a160e66405fd48fb7ba50eb63183cac0e4ed4f
hwanlee <ljhw3377@gmail.com>
2019-07-28 17:59:31 +0900

    Add: 이미지 클릭 시 클릭한 위치의 상대적 좌표를 출력 in MetroMap

--- metrotravelapp/src/components/MetroMap/index.js
+++ metrotravelapp/src/components/MetroMap/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./MetroMap.css";
 import metromap from '../../assets/images/metromap.png';
 
 const MetroMap = () => {
+  const imgRef = useRef(null);
+
+  const onClick = e => {
+    const { offsetX, offsetY } = e.nativeEvent;
+    const { width, height } = imgRef.current;
+    const rx = offsetX / width;
+    const ry = offsetY / height;
+
+    console.log(rx, ry);
+  }
+
   return (
     <div className="metromap">
-      <img src={metromap} alt="map" />
+      <img
+        src={metromap}
+        alt="map"
+        onClick={onClick}
+        ref={imgRef}
+      />
     </div>
   );
 };
